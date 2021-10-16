bf35e0be12dfd36d4d833859ddd81718bbcb8008
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-15 18:19:49 -0600

    Connected to MongoDB and test user is created. First test routes working.

--- server/index.js
+++ server/index.js
@@ -2,11 +2,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const api = express();
+const user = require('./models/user');
+
+mongoose.connect(
+  'mongodb+srv://cameronmccloskey:GnNvHp7c3zKtbTk@cluster0.bzmv9.mongodb.net/test',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => console.log('Mongodb connected'));
 
 api.use(cors());
 
 api.get('/', (req, res) => {
-  res.json('Successful endpoint');
+  user.find({}, (err, users) => {
+    err ? res.json(err) : res.json(users);
+  });
+});
+
+api.get('/create', (req, res) => {
+  user.create({ username: 'test', password: 'test2' }, (err, user) =>
+    err ? res.json(err) : res.json(user)
+  );
+});
+
+api.get('/login', (req, res) => {
+  const user = req.query;
+  res.json(user);
 });
 
 api.listen(5000, () => console.log('Api listening'));

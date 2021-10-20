73e6834daea79a3ff80ebc46c8ae6bd6606e87e7
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-19 21:04:15 -0600

    Successful register user functionality into MongoDB database with our promise in the auth route.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('Auth route');
+// REGISTER
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;

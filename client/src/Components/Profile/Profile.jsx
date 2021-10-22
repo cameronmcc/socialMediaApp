39b9d7690734d70e0c6ffaa1ffdbe512cde944a7
Cameron McCloskey <camsthrowaway89@gmail.com>
2021-10-22 17:50:46 -0600

    Profile section designed. Time to connect our backend.

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -5,11 +5,22 @@ const Profile = () => {
   return (
     <div className='profile'>
       <div className='profile__wrapper'>
-        <div className='profile__top'></div>
-        <div className='profile__middle'></div>
+        <div className='profile__top'>
+          <h2 className='profile--item'>Your Profile</h2>
+          <img className=' profile--pic' src='bio.png' alt='bio.png' />
+          <div className='profile--item profile--top--heading'>
+            Cameron McCloskey
+          </div>
+        </div>
+        <div className='profile__middle'>
+          About me:
+          <div className='about--me'>
+            Full Stack React/MERN developer in Los Angeles. Coding/learning
+            every day 🤩
+          </div>
+        </div>
         <div className='profile__bottom'></div>
       </div>
-      <h1>Profile page</h1>
     </div>
   );
 };
